65510f411c0ad0566d10d9c7ff2ca18db5027e38
nix-pb <2321300@ub.edu.ph>
2024-11-06 17:59:40 +0800

    Update DatabaseHelper.java

--- PrepMate/app/src/main/java/com/example/prepmate/DatabaseHelper.java
+++ PrepMate/app/src/main/java/com/example/prepmate/DatabaseHelper.java
@@ -41,25 +41,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_NOTE_INGREDIENTS = "ingredients";
     public static final String COLUMN_NOTE_PROCEDURES = "procedures";
 
-    // Database structure; Table: Recipe
+    //Recipe
     public static final String COLUMN_RECIPE_ID = "recipe_id";
-    public static final String COLUMN_USER_ID = "user_id";
-    public static final String COLUMN_CATEGORY_ID = "category_id";
-    public static final String COLUMN_NOTE_ID = "note_id";
-    public static final String COLUMN_NOTE_TITLE = "title";
-    public static final String COLUMN_NOTE_START_TIME = "startTime";
-    public static final String COLUMN_NOTE_END_TIME = "endTime";
-    public static final String COLUMN_NOTE_INGREDIENTS = "ingredients";
-    public static final String COLUMN_NOTE_PROCEDURES = "procedures";
 
-    // Database structure; Table: Category
+    //Category
     public static final String COLUMN_CATEGORY_ID = "category_id";
     public static final String COLUMN_CATEGORY_NAME = "category_name";
 
-    // Database structure; Table: Calendar
+    //Calendar
     public static final String COLUMN_CALENDAR_ID = "calendar_id";
-    public static final String COLUMN_USER_ID = "user_id";
-    public static final String COLUMN_RECIPE_ID = "recipe_id";
     public static final String COLUMN_CALENDAR_SELECTEDDATE = "selectedDate";
     
     
